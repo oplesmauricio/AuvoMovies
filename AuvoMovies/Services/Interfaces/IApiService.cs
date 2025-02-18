0f5884eb6cac619001212dad5e2ddf618e587264
Mauricio Ribeiro <mauricio91araujo@gmail.com>
2025-02-18 14:05:01 -0300

    feat: ApiService

---
+++ AuvoMovies/Services/Interfaces/IApiService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuvoMovies.Services.Interfaces
+{
+    public interface IApiService
+    {
+        Task<RespostaServico<T>> PostAsync<T>(object item, string url, List<KeyValuePair<string, string>> headers);
+
+        Task<RespostaServico<T>> GetAsync<T>(string url, List<KeyValuePair<string, string>> headers);
+
+        Task<RespostaServico<T>> PutAsync<T>(object item, string url, List<KeyValuePair<string, string>> headers);
+
+        Task<RespostaServico<T>> DeleteAsync<T>(string url, List<KeyValuePair<string, string>> headers);
+
+        Task<RespostaServico<T>> PostAnonymousAsync<T>(object item, string url);
+    }
+}
---
+++ AuvoMovies/Services/RespostaServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuvoMovies.Services
+{
+    public class RespostaServico<T>
+    {
+        public string HttpStatus { get; set; }
+        public bool Sucesso { get; set; }
+        public string Mensagem { get; set; }
+        public T Resposta { get; set; }
+    }
+}
