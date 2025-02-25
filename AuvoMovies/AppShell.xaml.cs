dec56622188fb30cdd1d250caa550787b9f3ed99
Mauricio Ribeiro <mauricio91araujo@gmail.com>
2025-02-25 17:17:50 -0300

    feat: Menus

--- AuvoMovies/AppShell.xaml.cs
+++ AuvoMovies/AppShell.xaml.cs
@@ -13,5 +13,16 @@ namespace AuvoMovies
             Routing.RegisterRoute(nameof(FavoritosPage), typeof(FavoritosPage));
             Routing.RegisterRoute(nameof(EnviarNotificacaoPage), typeof(EnviarNotificacaoPage));
         }
+
+        protected override void OnAppearing()
+        {
+            //Nao consegui instalar nenhuma dll atraves do nuget que pudesse me ajudar com a notificacao do firebase para ios, implementei para android e vo ucontinuar pesquisando pra descobrir
+            //estava vendo q mais algumas pessoas estao com esta dificuldade https://stackoverflow.com/questions/79413766/cannot-install-plugin-firebase-cloudmessaging-in-net-maui-project-path-too-lon
+            //um dos erros eh o caminho dos arquivos https://developercommunity.visualstudio.com/t/Allow-building-running-and-debugging-a/351628
+            base.OnAppearing();
+#if IOS
+            var x = this.menu.Items.Remove(this.EnviarNotificacao);
+#endif
+        }
     }
 }
